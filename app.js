eea445ba62f767cdbb44681cf2ada434e204c72d
Dan Rogers <dan.rogers@bkwld.com>
2013-01-22 22:52:38 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , stylus = require('stylus')
+  , nib = require('nib')
+;
+
+var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .use(nib())
+    .import('nib');
+}
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(stylus.middleware({ src: __dirname + '/public' , compile: compile }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(nib());
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/:id?', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+var title = 'GIFMAX: Maxgif, but lighter.';
+
+function requestPath(id, callback) {
+  // get image path from HTML
+  request({ uri: 'http://maxgif.com/'+id }, function (error, response, body) {
+    if (error) {
+      callback();
+    } else {
+      var $ = cheerio.load(body);
+      callback($('head').find('meta[property="og:image"]').attr('content'));
+    }
+  });
+}
+
+function requestToken(callback) {
+  request({ uri: 'http://maxgif.com/r.json', json: true }, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    callback(body.token);
+  });
+}
+
+exports.index = function(req, res){
+  // load by specific id
+  if (req.params.id) {
+    requestToken(function (nextToken) {
+      console.log(nextToken);
+      requestPath(req.params.id, function (path) {
+        res.render('index', { title: title, img: path, token: '', link: nextToken });
+      });
+    });
+  // or load random tokens
+  } else {
+    requestToken(function (thisToken) {
+      requestToken(function (nextToken) {
+        requestPath(thisToken, function (path) {
+          res.render('index', { title: title, img: path, token: thisToken, link: nextToken});
+        });
+      });
+    });
+  }
+};
