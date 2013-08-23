fcba71d6577e799a359fefa8c36d6bb3960c62fa
Dan Rogers <dan.rogers@bkwld.com>
2013-08-23 11:34:39 -0700

    Fix standalone on iOS

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,20 +1,33 @@
 (function () {
-	
-	document.onkeydown = function (evt) {
-		evt = evt || window.event;
-		switch (evt.keyCode) {
-			case 37: return back();
-			case 32: case 39: return next();
-		}
-	};
-	
-	function back() {
-		history.go(-1);
-	}
-	
-	function next() {
-		var el = document.querySelector("a");
-		window.location.href = el.attributes.href.value;
-	}
-	
+  
+  var link = document.querySelector('a');
+  var href = link.getAttribute('href');
+  
+  document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+      case 37: return back();
+      case 32: case 39: return next();
+    }
+  };
+  
+  function back() {
+    history.go(-1);
+    return false;
+  }
+  
+  function next() {
+    document.location.href = href;
+    return false;
+  }
+  
+  // Mobile Safari in standalone mode
+  // https://gist.github.com/kylebarrow/1042026
+  if (("standalone" in window.navigator) && window.navigator.standalone) {
+    link.ontouchstart = function () {
+      event.preventDefault();
+      document.location.href = href;
+    };
+  }
+  
 }());
